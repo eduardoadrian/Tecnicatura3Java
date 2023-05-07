f621f7f6890e3b70960eee28662b15b804b0197d
eduardoadrian <104809718+eduardoadrian@users.noreply.github.com>
2023-05-07 15:32:41 -0300

    Clase 3 leccion03
    clase3- leccion03-forEach,Autoboxing,Unboxing,Modificadores de acceso

---
+++ Leccion03/ModificadoresdeAcceso/src/paquete1/Clase2.java
@@ -0,0 +1,22 @@
+
+package paquete1;
+
+
+class Clase2 extends Clase1{
+    String atributoDefault = "Valor delatributo default";
+    
+    //Clase2(){
+      //  System.out.println("Constructor Default");
+    //}
+    
+    public Clase2(){
+        super();
+        this.atributoDefault = "Modificacion del atributo default";
+        System.out.println("atributoDefault = " + this.atributoDefault);
+        this.metodoDefault();
+    }
+    
+    void metodoDefault(){
+        System.out.println("Metodo Default");
+    }
+}
