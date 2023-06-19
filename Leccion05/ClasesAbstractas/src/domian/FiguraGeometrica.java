89f32cf11aae691393a45c99f211f7807eb2275a
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-06-19 02:12:35 -0300

    Leccion05-Conversion de Objetos,Downcasting,Upcasting,hashcode,equals

---
+++ Leccion05/ClasesAbstractas/src/domian/FiguraGeometrica.java
@@ -0,0 +1,35 @@
+
+package domian;
+
+public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+    
+    protected FiguraGeometrica(String tipoFigura){
+        this.tipoFigura = tipoFigura;
+    }
+    //Método abstracto
+    public abstract void dibujar();
+    
+    //Agregamos el metodo get y set
+    
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+    
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+    
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" + "tipoFigura=" + tipoFigura + '}';
+    }
+
+}    
+    
+    
+    
+    
+    
+    
+}
